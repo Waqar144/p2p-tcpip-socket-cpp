2331afdffd6773668b037635c968345c230aac52
Waqar Ahmed <waqar.17a@gmail.com>
2020-03-30 00:49:12 +0500

    Fix socket resource not building on windows

--- src/SocketResource.cpp
+++ src/SocketResource.cpp
@@ -1,14 +1,16 @@
 #include "SocketResource.hpp"
 
-SocketResource::SocketResource(Socket sockFd) : m_socket(sockFd) {}
+SocketResource::SocketResource(Socket sockFd) : m_socket(sockFd)
+{
+}
 
 int SocketResource::init()
 {
 #ifdef _WIN32
     WSADATA wsaData;
-    return WSAStartup(MAKEWORD(2,2), &wsaData);
+    return WSAStartup(MAKEWORD(2, 2), &wsaData);
 #else
-    return N0_ERROR;
+    return NO_ERROR;
 #endif
 }
 
@@ -17,23 +19,22 @@ int SocketResource::cleanUp()
 #ifdef _WIN32
     return WSACleanup();
 #else
-    return N0_ERROR;
+    return NO_ERROR;
 #endif
 }
 
-int SocketResource::close(Socket sock)
+bool SocketResource::close()
 {
-    int status = N0_ERROR;
+    if (m_socket == INVALID_SOCKET)
+        return false;
 
-    #ifdef _WIN32
-      status = shutdown(sock, SD_BOTH);
-      if (status == N0_ERROR) { status = closesocket(sock); }
-    #else
-      status = shutdown(sock, SHUT_RDWR);
-      if (status == N0_ERROR) { status = close(sock); }
-    #endif
+#ifdef _WIN32
+    int status = closesocket(m_socket);
+#else
+    int status = ::close(m_socket);
+#endif
 
-    return status;
+    return status != SOCKET_ERROR;
 }
 
 bool SocketResource::setNonBlockingMode()
@@ -41,10 +42,11 @@ bool SocketResource::setNonBlockingMode()
 #ifdef _WIN32
     u_long iMode = 1;
     const bool ret = N0_ERROR == ioctlsocket(m_socket, FIONBIO, &iMode);
-#endif
+#else
     const int flags = fcntl(m_socket, F_GETFL, 0);
-    const bool ret = N0_ERROR == fcntl(m_socket, F_SETFL, flags | O_NONBLOCK);
+    const bool ret = NO_ERROR == fcntl(m_socket, F_SETFL, flags | O_NONBLOCK);
 
+#endif
     return ret;
 }
 
@@ -53,29 +55,38 @@ bool SocketResource::setBlockMode()
 #ifdef _WIN32
     u_long iMode = 0;
     const bool ret = N0_ERROR == ioctlsocket(m_socket, FIONBIO, &iMode);
-#endif
-
+#else
     const int flags = fcntl(m_socket, F_GETFL, 0);
-    const bool ret = N0_ERROR == fcntl(m_socket, F_SETFL, flags & ~O_NONBLOCK);
+    const bool ret = NO_ERROR == fcntl(m_socket, F_SETFL, flags & ~O_NONBLOCK);
+#endif
 
     return ret;
 }
 
+bool SocketResource::SetSocketNoDelay()
+{
+    int set = 1;
+    int rc = setsockopt(m_socket, IPPROTO_TCP, TCP_NODELAY, (const char *)&set, sizeof(int));
+    return rc == 0;
+}
+
 SocketResource SocketResource::create()
 {
-    if (init() != N0_ERROR) {
+    if (init() != NO_ERROR) {
         cleanUp();
         return SocketResource(INVALID_SOCKET);
     }
 
     Socket sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock == INVALID_SOCKET)
-    {
+    if (sock == INVALID_SOCKET) {
         return SocketResource(INVALID_SOCKET);
     }
-    return SocketResource(sock);
+    SocketResource rsc(sock);
+    rsc.SetSocketNoDelay();
+    return rsc;
 }
 
-Socket SocketResource::resource() const {
+Socket SocketResource::resource() const
+{
     return m_socket;
 }
--- src/SocketResource.hpp
+++ src/SocketResource.hpp
@@ -1,24 +1,30 @@
 #pragma once
 
 #ifdef _WIN32
-    /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
-    #ifndef _WIN32_WINNT
-        #define _WIN32_WINNT 0x0501  /* Windows XP. */
-    #endif
-    #include <winsock2.h>
-    #include <Ws2tcpip.h>
+/* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501 /* Windows XP. */
+#endif
+#include <Ws2tcpip.h>
+#include <winsock2.h>
 
-    #define SHUT_RDWR SD_BOTH
+#ifdef _MSC_VER
+#pragma comment(lib, "Ws2_32.lib")
+#endif
+
+#define SHUT_RDWR SD_BOTH
 #else
-  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
-#include <sys/socket.h>
+/* Assume that any non-Windows platform uses POSIX-style sockets instead. */
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#define N0_ERROR 0
+#define NO_ERROR 0
 #define INVALID_SOCKET -1
-
+#define SOCKET_ERROR -1
 #endif
 
 // MSG_DONTWAIT is not available on some platforms, if it doesn't exist define it as 0
@@ -30,16 +36,17 @@ using Socket = decltype(socket(0, 0, 0));
 
 class SocketResource
 {
-private:
+  private:
     Socket m_socket;
 
-public:
+  public:
     SocketResource(Socket sockFd = -1);
     static int init();
     static int cleanUp(void);
-    static int close(Socket sock);
+    bool close();
     bool setNonBlockingMode();
     bool setBlockMode();
     static SocketResource create();
     Socket resource() const;
+    bool SetSocketNoDelay();
 };
