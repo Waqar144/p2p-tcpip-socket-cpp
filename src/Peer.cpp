e0e3a673ea239845ae6639c7e77135ee6f2b571c
Waqar Ahmed <waqar.17a@gmail.com>
2020-03-30 00:54:41 +0500

    Use vector for socket recv()

--- src/Peer.cpp
+++ src/Peer.cpp
@@ -2,21 +2,22 @@
 #include "P2PSocket.hpp"
 #include "exceptions/peerexception.h"
 
-#include <unistd.h>
 #include <iostream>
+#include <unistd.h>
 
 #include <cstring>
-#include <utility> 
+#include <utility>
 
-Peer::Peer(std::shared_ptr<P2PSocket> socket, SocketResource peer, int num) : m_master(std::move(socket)), m_socket(peer)
+Peer::Peer(P2PSocket *socket, SocketResource peer, int num)
+    : m_master(socket), m_socket(peer), m_flags{0}
 {
     struct sockaddr_in address;
     std::memset(&address, 0, sizeof address);
     socklen_t len = sizeof(address);
-    int ret = ::getpeername(peer.resource(), (struct sockaddr*)&address, &len);
+    int ret = ::getpeername(peer.resource(), (struct sockaddr *)&address, &len);
     if (ret == -1)
-        std::cout<< "New peer connection " + std::to_string(num) + " failed." << std::endl;
-    m_port = ::ntohs(address.sin_port);
+        std::cout << "New peer connection " + std::to_string(num) + " failed." << std::endl;
+    m_port = ntohs(address.sin_port);
     m_ip = ::inet_ntoa(address.sin_addr);
     m_name = m_ip + ":" + std::to_string(m_port);
     m_connected = true;
@@ -27,19 +28,23 @@ Peer::~Peer()
     m_socket.close();
 }
 
-std::string Peer::name() const {
+std::string Peer::name() const
+{
     return m_name;
 }
 
-std::string Peer::ip () const {
+std::string Peer::ip() const
+{
     return m_ip;
 }
 
-uint16_t Peer::port() const {
+uint16_t Peer::port() const
+{
     return m_port;
 }
 
-SocketResource Peer::socket() const {
+SocketResource Peer::socket() const
+{
     return m_socket;
 }
 
@@ -47,32 +52,34 @@ int Peer::send(const std::string &message) const
 {
 #ifdef _WIN32
     /*write can be problematic on windows, using send() here */
-    ::send(m_socket.resource(), message.c_str(), message.size(), 0);
+    int ret = ::send(m_socket.resource(), message.c_str(), message.size(), 0);
 #else
     int ret = ::write(m_socket.resource(), message.c_str(), message.size());
 #endif
     if (ret == -1) {
-        throw PeerException(std::string{"Failed to write to peer "} + m_name + " " + std::strerror(errno) + "\n");
+        throw PeerException(std::string{"Failed to write to peer "} + m_name + " " +
+                            std::strerror(errno) + "\n");
     }
 
     return ret;
 }
-
-std::string Peer::read(int length)
+std::string Peer::read(const int length)
 {
-    char buf[length];
-    std::memset(buf, 0, sizeof buf);
-    int recv = ::recv(m_socket.resource(), buf, length, MSG_DONTWAIT);
+    std::vector<char> buf(length);
+    int recv = ::recv(m_socket.resource(), &buf[0], length, MSG_DONTWAIT);
     if (recv == -1) {
-        throw PeerException(std::string{"Failed to read peer "} + m_name + " " + std::strerror(errno) + "\n");
-     }
-    if (recv == 0) {
-        //Connection is no longer valid, remote has been disconnected
-        m_connected = false;
-        this->m_master->peers()->remove(this);
-        this->m_master->events()->onPeerDisconnect()->trigger(this);
+        throw PeerException(std::string{"Failed to read peer "} + m_name + " " +
+                            std::strerror(errno) + "\n");
     }
-    return buf;
+    if (recv == 0) {
+        // Connection is no longer valid, remote has been disconnected
+        m_connected = false;
+        this->m_master->events()->onPeerDisconnect()->trigger(this);
+        this->m_master->peers()->remove(this);
+        return std::string{};
+    }
+    buf.resize(recv);
+    return std::string{buf.data()};
 }
 
 bool Peer::status() const
@@ -80,15 +87,26 @@ bool Peer::status() const
     return m_connected;
 }
 
-void Peer::disconnect()
+void Peer::disconnect(bool suppressException)
 {
     int shutdown = ::shutdown(m_socket.resource(), SHUT_RDWR);
-    if (shutdown == -1) {
-        throw PeerException(std::string{"Failed to shutdown socket to peer "} + m_name + " " + std::strerror(errno) + "\n");
+    if (shutdown == -1 && !suppressException) {
+        throw PeerException(std::string{"Failed to shutdown socket to peer "} + m_name + " " +
+                            std::strerror(errno) + "\n");
     }
 
-    //Don't call close here, it will be triggered automatically when object gets destroyed
+    // Don't call close here, it will be triggered automatically when object gets destroyed
     m_connected = false;
-    this->m_master->peers()->remove(this);
     this->m_master->events()->onPeerDisconnect()->trigger(this);
+    this->m_master->peers()->remove(this);
+}
+
+uint8_t Peer::flags() const
+{
+    return m_flags;
+}
+
+void Peer::setFlags(uint8_t flags)
+{
+    m_flags = flags;
 }
--- src/Peer.hpp
+++ src/Peer.hpp
@@ -1,33 +1,38 @@
 #pragma once
 
 #include "SocketResource.hpp"
-#include <string>
 #include <memory>
+#include <string>
 
 class P2PSocket;
 
-class Peer {
-public:
+class Peer
+{
+  public:
     Peer() = default;
-    Peer(std::shared_ptr<P2PSocket> socket, SocketResource peer, int num);
+    Peer(P2PSocket *socket, SocketResource peer, int num);
     ~Peer();
     std::string name() const;
-    std::string ip () const;
+    std::string ip() const;
     uint16_t port() const;
     SocketResource socket() const;
 
     int send(const std::string &message) const;
-    std::string read(int length = 1024);
+    std::string read(const int length = 1024);
 
     bool status() const;
 
-    void disconnect();
+    void disconnect(bool suppressException = false);
 
-private:
-    std::shared_ptr<P2PSocket> m_master;
-    std::string m_ip;
-    uint16_t m_port;
+    uint8_t flags() const;
+    void setFlags(uint8_t flags);
+
+  private:
+    P2PSocket *m_master;
     std::string m_name;
+    std::string m_ip;
     SocketResource m_socket;
+    uint16_t m_port;
+    uint8_t m_flags;
     bool m_connected;
 };
